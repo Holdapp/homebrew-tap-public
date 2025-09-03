2001021b75b2863d9801b4fb37a0e6cec4e41469
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2025-09-03 16:15:27 +0200

    Update locgen-swift to version 1.1.5
    - Fix rich text extraction from XLSX shared strings
    - Resolve missing translations stored as formatted text
    - Improve shared string cell handling for complex content
    
    Commit: dd97cde758d0a8b0a98d5b6ee85b2a3a4b07c9e0

--- Formula/locgen-swift.rb
+++ Formula/locgen-swift.rb
@@ -1,7 +1,7 @@
 class LocgenSwift < Formula 
     desc "Localization strings generator for iOS/macOS projects"
     homepage "https://github.com/Holdapp/locgen-swift"
-    url "https://github.com/Holdapp/locgen-swift.git", tag: "1.1.4", revision: "417cf80a74f7e24b1756109a47bcb298a52fe4d6"
+    url "https://github.com/Holdapp/locgen-swift.git", tag: "1.1.5", revision: "dd97cde758d0a8b0a98d5b6ee85b2a3a4b07c9e0"
     head "https://github.com/Holdapp/locgen-swift.git"
 
     depends_on xcode: ["13.0", :build]
