f198fa2823fb0c687fb181e36137460920337cf1
Łukasz Szyszkowski <lukasz.szyszkowski@holdapp.pl>
2025-09-03 16:18:54 +0200

    Fix SHA for locgen-swift 1.1.5
    Correct revision SHA to match actual commit:
    dd97cdeec118eb7e75581c48ecbd7c6d98267439

--- Formula/locgen-swift.rb
+++ Formula/locgen-swift.rb
@@ -1,7 +1,7 @@
 class LocgenSwift < Formula 
     desc "Localization strings generator for iOS/macOS projects"
     homepage "https://github.com/Holdapp/locgen-swift"
-    url "https://github.com/Holdapp/locgen-swift.git", tag: "1.1.5", revision: "dd97cde758d0a8b0a98d5b6ee85b2a3a4b07c9e0"
+    url "https://github.com/Holdapp/locgen-swift.git", tag: "1.1.5", revision: "dd97cdeec118eb7e75581c48ecbd7c6d98267439"
     head "https://github.com/Holdapp/locgen-swift.git"
 
     depends_on xcode: ["13.0", :build]
